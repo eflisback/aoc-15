ee55b5423d8c63833336cb20638aa7affde09e07
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2026-05-09 18:30:13 +0200

    5b progress

--- src/bin/5.rs
+++ src/bin/5.rs
@@ -4,6 +4,8 @@ fn main() {
     let input = include_str!("../../data/5.txt").trim();
     let a = a(input);
     println!("a: {a}");
+    let b = b(input);
+    println!("b: {b}");
 }
 
 fn has_three_vowels(line: &str) -> bool {
@@ -31,5 +33,20 @@ fn a(input: &str) -> usize {
                 && has_same_letter_twice_in_a_row(line)
                 && has_no_forbidden_substrings(line)
         })
+        .count()
+}
+
+fn has_repeated_pair(line: &str) -> bool {
+    true
+}
+
+fn has_repeated_character_with_one_in_between(line: &str) -> bool {
+    line.chars().zip(line.chars().skip(2)).any(|(a, b)| a == b)
+}
+
+fn b(input: &str) -> usize {
+    input
+        .split("\n")
+        .filter(|line| has_repeated_pair(line) && has_repeated_character_with_one_in_between(line))
         .count()
 }
